1491bd6f8bfaf2c8871e7b52170ede2ae59b23d6
marmagzzz <marvin@xtendly.com>
2020-08-31 20:52:59 +0800

    fix CORS

--- server/CORS/index.js
+++ server/CORS/index.js
@@ -1,6 +1,6 @@
 const CORS = require('cors');
 
-const allowedOrigins = ['https://*.herokuapp.com/, http://127.0.0.1:8000, http://localhost:8000', 'http://localhost:5000', 'http://127.0.0.1:5000', 'http://localhost:3000', 'http://127.0.0.1:3000'];
+const allowedOrigins = ['https://*.herokuapp.com, http://127.0.0.1:8000, http://localhost:8000', 'http://localhost:5000', 'http://127.0.0.1:5000', 'http://localhost:3000', 'http://127.0.0.1:3000'];
 
 module.exports = CORS({
     origin: function(origin, callback){
