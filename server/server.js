b2af4e1c352b2edca8da0e3d7b7dfbf51dcf2f61
marmagzzz <marvin@xtendly.com>
2020-08-31 21:24:22 +0800

    fix CORS

--- server/server.js
+++ server/server.js
@@ -90,9 +90,7 @@ if (process.env.NODE_ENV === "production") {
 
     /** INDEX ROUTE */
     const IndexRoute = require('./routes/index');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-    });
+    app.use('/', IndexRoute);
 
 /** END OF ROUTE REGISTRATION */
 
