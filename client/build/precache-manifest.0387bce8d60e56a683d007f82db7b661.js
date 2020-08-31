7b5acb115e4b5675024312765e70ad83bcb1adde
marmagzzz <marvin@xtendly.com>
2020-08-31 20:05:25 +0800

    include client build folder for heroku deployment

---
+++ client/build/precache-manifest.0387bce8d60e56a683d007f82db7b661.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1772be66a1f0700ce5e326db3eb451",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3962cbe9cc4f086e1c0b",
+    "url": "/static/css/2.3b460f5c.chunk.css"
+  },
+  {
+    "revision": "33709ada2b5463525931",
+    "url": "/static/css/main.a1ead8a2.chunk.css"
+  },
+  {
+    "revision": "3962cbe9cc4f086e1c0b",
+    "url": "/static/js/2.41c570b9.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.41c570b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33709ada2b5463525931",
+    "url": "/static/js/main.681043da.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
