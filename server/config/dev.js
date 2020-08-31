d9fdfd352241d89cb61d0ac509733b693bf55d36
marmagzzz <marvin@xtendly.com>
2020-08-31 19:43:12 +0800

    update database config

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI : 'mongodb+srv://admin:admin@prosperna.fgxk2.mongodb.net/prospernadb?retryWrites=true&w=majority',
+    mongoURI : 'mongodb+srv://admin:marmagz>@bentamonatol.z8n1x.mongodb.net/bentamonatol?retryWrites=true&w=majority',
     SERVER_PORT : 5000
 };
